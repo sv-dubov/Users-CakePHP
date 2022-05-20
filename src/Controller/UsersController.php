396ba2a8ac3aa1e83037a7788b9e421ce58d5ca3
Sviatoslav <sv_dubov@ukr.net>
2022-05-20 18:30:01 +0300

    Fix self deleting in backend

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -68,10 +68,15 @@ class UsersController extends AppController
     {
         $this->request->allowMethod(['post', 'delete']);
         $user = $this->Users->get($id);
-        if ($this->Users->delete($user)) {
-            $this->Flash->success(__('The user has been deleted.'));
+        $identity = $this->request->getAttribute('identity');
+        if ($identity->id == $id) {
+            $this->Flash->error(__('You cannot deleted yourself.'));
         } else {
-            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+            if ($this->Users->delete($user)) {
+                $this->Flash->success(__('The user has been deleted.'));
+            } else {
+                $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+            }
         }
         return $this->redirect(['action' => 'index']);
     }
